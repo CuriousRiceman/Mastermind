cd9ae5fa99767de4819c8c638ec34028a0932afc
Richman <richmanong4@gmail.com>
2023-12-27 22:10:16 -1000

    Nearly finished, just implemented and debugged the hints portion and the win portion of the codebreaker

--- mastermind.rb
+++ mastermind.rb
@@ -29,18 +29,21 @@ class Mastermind
                 end
             end
             puts "\nSelected colors: #{@code.join(', ')}"
-            user_input_valid == true
+            user_input_valid = true
         else
+            puts " "
             puts "Computer is the mastermind. You are the code breaker. "
             @code = COLORS.sample(4)
             p code
         end
+        #call computer solving algorithm and compare to see if it guess code correctly
+        #then do win lose statements
     end
 
 end
 
 class CodeBreaker
-    include COLORS
+    include Colors
 
     attr_accessor :who_is_the_codebreaker, :code_guessed
 
@@ -55,12 +58,11 @@ class CodeBreaker
     end
 
     def guess(code)
-        for i in 1..@rounds
-            if player_or_computer == "Player"
-                user_input_valid = false
-                puts "You are the mastermind. Computer is the code breaker.\n "
-                puts "Given these options: "
-                puts "(R) Red, (B) Blue, (O) Orange, (Y) Yellow, (G) Green, (P) Purple\n "
+        if @who_is_the_codebreaker == "Player"
+            user_input_valid = false
+            puts "Given these options: "
+            puts "(R) Red, (B) Blue, (O) Orange, (Y) Yellow, (G) Green, (P) Purple\n "
+            for i in 1..@rounds
                 @code_guessed.each_with_index do |_, slot|
                     loop do
                         puts "Choose a color for slot #{slot + 1}: "
@@ -73,40 +75,37 @@ class CodeBreaker
                         end
                     end
                 end
-                
                 if @code_guessed == code
-                    guessed = true
+                    puts "You win!"
+                    #input to ask if they want to play again 
+                    break
                 else
                     array_hint = Array.new(4, " ")
                     @code_guessed.each_with_index do |value, slot|
                         if @code_guessed[slot] == code[slot]
-                            array_hint[slot] >> "B"
+                            array_hint[slot] = "B"
                         elsif code.include?(@code_guessed[slot])
-                            array_hint[slot] >> "W"
+                            array_hint[slot] = "W"
                         end
                     end
                     puts "Round#{i}: (#{@code_guessed[0]}) (#{@code_guessed[1]}) (#{@code_guessed[2]}) (#{@code_guessed[3]})"
-                    if black.length != 0
-
+                    array_hint_sorted = array_hint.sort_by { |element| ["B", "W", " "].index(element) || 3 }
+                    puts "Hints: (#{array_hint_sorted[0]}) (#{array_hint_sorted[1]}) (#{array_hint_sorted[2]}) (#{array_hint_sorted[3]})"
+                    puts " "
                 end
-                
-            else
-                #computer solving algorithm
             end
-            i++
+            puts "You lose!"
+            #input to ask if they want to play again 
+        else
+            #computer solving algorithm
         end
-
+            
     end
-
-
-
-
 end
+
 
 class Play
     attr_accessor :player, :computer
-
-    ROUNDS = 10
 
     def initialize()
         loop do
@@ -119,7 +118,7 @@ class Play
                 else
                     @computer = Mastermind.new("Computer")
                     @player = CodeBreaker.new("Player")
-                    player.guess
+                    @player.guess(@computer.code)
                 end
                 break
             else
@@ -127,7 +126,15 @@ class Play
             end
         end
     end
-                
+    
+    def play_again?()
+        loop do
+            puts "Do you want to play again? (Y/N)"
+            answer = gets.chomp.upcase
+            if answer = "N"
+                puts "Thanks for playing! Goodbye."
+            elsif answer = "Y"
+                initialize()
 end
 
-play = Play.new
+Play.initialize
