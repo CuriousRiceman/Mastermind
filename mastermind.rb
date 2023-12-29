0b8783807bcd1803cb82b7f1049f51e54b59fac8
Richman <richmanong4@gmail.com>
2023-12-28 21:39:09 -1000

    Finished computer solving algorithm, can definitely optimize it to win in less rounds, however, I will leave it up to chance

--- mastermind.rb
+++ mastermind.rb
@@ -12,7 +12,6 @@ class Mastermind
         @who_is_the_mastermind = player_or_computer
         @code = Array.new(4)
         if player_or_computer == "Player"
-            user_input_valid = false
             puts "You are the mastermind. Computer is the code breaker. "
             @code.each_with_index do |_, slot|
                 loop do
@@ -29,15 +28,12 @@ class Mastermind
                 end
             end
             puts "\nSelected colors: #{@code.join(', ')}"
-            user_input_valid = true
         else
             puts " "
             puts "Computer is the mastermind. You are the code breaker. "
             @code = COLORS.sample(4)
             p code
         end
-        #call computer solving algorithm and compare to see if it guess code correctly
-        #then do win lose statements
     end
 
 end
@@ -50,7 +46,7 @@ class CodeBreaker
     def initialize(player_or_computer)
         @who_is_the_codebreaker = player_or_computer
         @code_guessed = Array.new(4)
-        @rounds = 10
+        @rounds = 12
     end
 
     def solve()
@@ -65,7 +61,7 @@ class CodeBreaker
             for i in 1..@rounds
                 @code_guessed.each_with_index do |_, slot|
                     loop do
-                        puts "Choose a color for slot #{slot + 1}: "
+                        puts "Choose a color for slot #{slot + 1} (NO REPEATING COLORS): "
                         color = gets.chomp.upcase
                         if COLORS.include?(color)
                             @code_guessed[slot] = color
@@ -77,7 +73,7 @@ class CodeBreaker
                 end
                 if @code_guessed == code
                     puts "You win!"
-                    #input to ask if they want to play again 
+                    Play.play_again?
                     break
                 else
                     array_hint = Array.new(4, " ")
@@ -92,12 +88,42 @@ class CodeBreaker
                     array_hint_sorted = array_hint.sort_by { |element| ["B", "W", " "].index(element) || 3 }
                     puts "Hints: (#{array_hint_sorted[0]}) (#{array_hint_sorted[1]}) (#{array_hint_sorted[2]}) (#{array_hint_sorted[3]})"
                     puts " "
+                    if i == 12
+                        puts "Computer wins! You lose!"
+                        Play.play_again?
+                    end
                 end
             end
-            puts "You lose!"
-            #input to ask if they want to play again 
-        else
-            #computer solving algorithm
+        else #Computer solving algorithm
+            colors = COLORS
+            for i in 1..@rounds
+                @code_guessed = colors.sample(4)
+                if @code_guessed == code
+                    puts "Computer wins!"
+                    Play.play_again?
+                    break
+                else
+                    array_hint = Array.new(4, " ")
+                    @code_guessed.each_with_index do |value, slot|
+                        if @code_guessed[slot] == code[slot]
+                            array_hint[slot] = "B"
+                        elsif code.include?(@code_guessed[slot])
+                            array_hint[slot] = "W"
+                        else
+                            colors.delete(@code_guessed[slot])
+                        end
+                    end
+                    puts "Round#{i}: (#{@code_guessed[0]}) (#{@code_guessed[1]}) (#{@code_guessed[2]}) (#{@code_guessed[3]})"
+                    array_hint_sorted = array_hint.sort_by { |element| ["B", "W", " "].index(element) || 3 }
+                    puts "Hints: (#{array_hint_sorted[0]}) (#{array_hint_sorted[1]}) (#{array_hint_sorted[2]}) (#{array_hint_sorted[3]})"
+                    puts " "
+                    if i == @rounds
+                        puts "Player wins! Computer loses!"
+                        Play.play_again?
+                    end
+                end
+            end
+            
         end
             
     end
@@ -107,7 +133,7 @@ end
 class Play
     attr_accessor :player, :computer
 
-    def initialize()
+    def self.setup_game()
         loop do
             puts "Do you want to be the (1) mastermind or (2) code breaker? "
             role_num = gets.chomp.to_i
@@ -115,6 +141,7 @@ class Play
                 if role_num == 1
                     @player = Mastermind.new("Player")
                     @computer = CodeBreaker.new("Computer")
+                    @computer.guess(@player.code)
                 else
                     @computer = Mastermind.new("Computer")
                     @player = CodeBreaker.new("Player")
@@ -126,15 +153,22 @@ class Play
             end
         end
     end
-    
-    def play_again?()
+
+    def self.play_again?
         loop do
-            puts "Do you want to play again? (Y/N)"
-            answer = gets.chomp.upcase
-            if answer = "N"
-                puts "Thanks for playing! Goodbye."
-            elsif answer = "Y"
-                initialize()
+          puts "Do you want to play again? (Y/N)"
+          answer = gets.chomp.upcase
+          if answer == "N"
+            puts "Thanks for playing! Goodbye."
+            break
+          elsif answer == "Y"
+            setup_game
+            break
+          else
+            puts "Invalid input, please enter 'Y' or 'N'."
+          end
+        end
+    end
 end
 
-Play.initialize
+Play.setup_game
